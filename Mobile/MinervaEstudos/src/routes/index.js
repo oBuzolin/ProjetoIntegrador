4746ebf0821d893ce9f50f3d97a1d84c59e52e36
oBuzolin <edubgamesbr@hotmail.com>
2023-12-05 22:01:46 -0300

    improving the code

--- Mobile/MinervaEstudos/src/routes/index.js
+++ Mobile/MinervaEstudos/src/routes/index.js
@@ -1,19 +1,20 @@
-import { createBottomNavigation } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from 'react-native-paper/react-navigation';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { Materias } from "../pages/materias";
-import { Pendentes } from "../pages/pendentes";
-import { Perfil } from "../pages/perfil";
-import { Ranking } from "../pages/ranking"; 
+import { Materias } from '../pages/materias'
+import { Pendentes } from '../pages/pendentes' 
+import { Perfil } from '../pages/perfil'
+import { Ranking } from '../pages/ranking'
 
-const Tab = createBottomNavigation();
+const Tab = createBottomTabNavigator();
 
 export function Routes() {
-    return(
-        <Tab.Navigator>        
-            <Tab.Screen name="Materias" component={Materias}/>
-            <Tab.Screen name="Pendentes" component={Pendentes}/>
-            <Tab.Screen name="Ranking" component={Ranking}/>
-            <Tab.Screen name="Perfil" component={Perfil}/>
-        <Tab.Navigator/>
+     return( 
+        <Tab.Navigator>
+            <Tab.Screen name="Materias" component={Materias} />
+            <Tab.Screen name="Pendentes" component={Pendentes} />
+            <Tab.Screen name="Ranking" component={Ranking} /> 
+            <Tab.Screen name="Perfil" component={Perfil} />
+        </Tab.Navigator>
     );
 }
