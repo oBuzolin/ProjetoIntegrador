421e87180edbba55e074fef3d6d94a947bb99376
Maria Eduarda Camargo <cl201107@g.unicamp.br>
2024-08-09 17:14:53 -0300

    Enviando as telas da página princial + enviando telas da sala de aula

--- Web/Minerva/professor/urls.py
+++ Web/Minerva/professor/urls.py
@@ -1,7 +1,8 @@
 # app_professor/urls.py
 from django.urls import path
-from .views import professor_home
+from .views import professor_home, ProjetoIntegrador_4DSN
 
 urlpatterns = [
     path('', professor_home, name='professor_home'),
+    path('ProjetoIntegrador_4DSN.html', ProjetoIntegrador_4DSN, name='ProjetoIntegrador_4DSN'),
 ]
--- Web/Minerva/professor/views.py
+++ Web/Minerva/professor/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def professor_home(request):
-    return render(request, 'professor_home')
+    return render(request, 'professor_home.html')
+
+def ProjetoIntegrador_4DSN(request):
+    return render(request, 'ProjetoIntegrador_4DSN.html')
